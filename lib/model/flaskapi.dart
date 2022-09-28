2292b976b89d1ab7dbf96ee1dcbf6b2f082f3862
RiyaShah08 <riyashah8oct@gmail.com>
2022-09-28 07:17:20 +0530

    In progress

--- lib/model/flaskapi.dart
+++ lib/model/flaskapi.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
 
-fetchdata(String url)async{
-  http.Response reponse = await http.get(Uri.parse(url));
-  return reponse.body;
+fetchdata(String url) async {
+  http.Response response = await http.get(Uri.parse(url));
+  return response.body;
 }
--- lib/screening.dart
+++ lib/screening.dart
@@ -1,10 +1,43 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class ScreenTest extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:sldchecker/model/flaskapi.dart';
+
+class ScreenTest extends StatefulWidget {
   const ScreenTest({Key? key}) : super(key: key);
 
   @override
+  State<ScreenTest> createState() => _ScreenTestState();
+}
+
+class _ScreenTestState extends State<ScreenTest> {
+
+  String url = '';
+  var data;
+  String output = 'Output';
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                url = 'http://86ff-35-243-172-89.ngrok.io/' + value.toString();
+              },
+            ),
+            TextButton(onPressed: ()async{
+              data = await fetchdata(url);
+              var decoded = jsonDecode(data);
+               setState(() {
+                 output = decoded['Prediction'];
+               });
+            }, child: Text("Predict")),
+            Text(output),
+          ],
+        ),
+      ),
+    );
   }
 }
