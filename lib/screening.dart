283a861a477db9cd6de28c0e26f861b66fc6ab73
RiyaShah08 <riyashah8oct@gmail.com>
2022-11-06 20:21:47 +0530

    In progress

--- lib/screening.dart
+++ lib/screening.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sldchecker/model/flaskapi.dart';
+
 
 class ScreenTest extends StatefulWidget {
   const ScreenTest({Key? key}) : super(key: key);
@@ -11,6 +14,21 @@ class ScreenTest extends StatefulWidget {
 }
 
 class _ScreenTestState extends State<ScreenTest> {
+
+  File? imageFile;
+
+  void _getFromCamera() async {
+    XFile? pickedFile = await ImagePicker().pickImage(
+        source: ImageSource.camera,
+        maxHeight: 1080,
+        maxWidth: 1080
+    );
+    setState(() {
+      imageFile = File(pickedFile!.path);
+    });
+    Navigator.pop(context);
+  }
+
 
   String url = '';
   var data;
@@ -22,6 +40,35 @@ class _ScreenTestState extends State<ScreenTest> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+
+            SizedBox(height: 50),
+            imageFile != null ?
+                Container(
+                  child: Image.file(imageFile!),
+                ):
+
+                Container(
+                  child: Icon(
+                    Icons.camera_enhance_rounded,
+                    color: Colors.black45,
+                    size: MediaQuery.of(context).size.width * .6,
+                  ),
+                ),
+            
+            Padding(
+              padding: const EdgeInsets.all(30),
+            child: ElevatedButton(
+              child: Text('Capture Image'),
+              onPressed: (){
+                _getFromCamera();
+              },
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+
+              ),
+            ),
+            ),
+            SizedBox(height: 20),
             TextField(
               onChanged: (value) {
                 url = 'http://41d5-35-234-2-165.ngrok.io/predict' + value.toString();
